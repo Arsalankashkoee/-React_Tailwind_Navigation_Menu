6f6b0c242e66915ec0663ef539f7b76df7ff2a2f
Arsalan Kashkoee <Arsalan.kashkoee@gmail.com>
2022-09-12 00:42:39 +0430

    initial message

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navigation from "./Components/Navigation";
+
+function App() {
+  return (
+    <div className="h-screen flex justify-center items-center bg-gray-900">
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
